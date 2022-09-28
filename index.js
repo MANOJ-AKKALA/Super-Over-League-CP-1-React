83e76838c692001a5b44a920f9826d1998ffa284
AkkalaManoj <amanojkg@gmail.com>
2022-09-28 17:12:28 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg">
+    <h3 className="heading">Super Over League</h3>
+
+    <div>
+      <img
+        className="rcb"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+
+      <img
+        className="csk"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
